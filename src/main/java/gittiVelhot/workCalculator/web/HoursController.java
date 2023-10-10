c881adc9f3ef314d8cae8092a8ef9c02095c00fc
NikoTikkanen <123297498+NikoTikkanen@users.noreply.github.com>
2023-10-10 18:08:34 +0300

    added thymeleaf test file for testing workshift adding

--- src/main/java/gittiVelhot/workCalculator/web/HoursController.java
+++ src/main/java/gittiVelhot/workCalculator/web/HoursController.java
@@ -1,11 +1,15 @@
 package gittiVelhot.workCalculator.web;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import gittiVelhot.workCalculator.domain.User;
 import gittiVelhot.workCalculator.domain.UserRepository;
@@ -19,6 +23,15 @@ public class HoursController {
 	private UserRepository urepository;
 	@Autowired
 	private WorkingHoursRepository wrepository;
+
+	
+	// home sivulta linkki jossa voi lisätä työvuoron userille.
+	//localhost:8080/homa
+	//localhost:8080/add
+    @RequestMapping("/home")
+    public String homePage() {
+        return "home"; // Palautetaan "home", joka viittaa home.html-sivun nimeen.
+    }
 	
 		@RequestMapping (value = "/api/user", method= RequestMethod.GET)
 		public @ResponseBody User userRest(Principal user){
@@ -35,10 +48,23 @@ public class HoursController {
 			return (List <WorkingHours>) wrepository.findAll();
 		}
 
-		//add work shift
-		@RequestMapping(value = "/save", method = RequestMethod.POST)
-		public String save(WorkingHours workingHours) {
-			wrepository.save(workingHours);
-			return "home";
+		//Lisää työvuoro
+		@RequestMapping(value = "/add")
+    	public String addHours(Model model) {
+		model.addAttribute("workingHours", new WorkingHours());
+
+        return "addHours"; 
 		}
+
+		//tallenna työvuoro
+		@RequestMapping(value = "/save", method = RequestMethod.POST)
+		public String save(@ModelAttribute("WorkingHours") WorkingHours workingHours, Principal principal){
+		String username = principal.getName();
+    	User user = urepository.findByUsername(username);
+
+    		// Aseta työntekijä käyttäjäksi
+    		workingHours.setUser(user);
+			wrepository.save(workingHours);
+			return "redirect:home";
+		}  
 }
