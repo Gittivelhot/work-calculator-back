873d0caf3b7276c8551871cebc0b985a8207ae34
attemkinen <atte.makinen1@outlook.com>
2023-11-09 13:17:27 +0200

    added CorsConfig.java file

---
+++ src/main/java/gittiVelhot/workCalculator/CorsConfig.java
@@ -0,0 +1,17 @@
+package gittiVelhot.workCalculator;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+	
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/json/**")
+      .allowedOrigins("*")
+      .allowedMethods("GET", "POST", "PUT", "DELETE")
+      .allowedHeaders("*")
+      .maxAge(3600);
+  }
+}
